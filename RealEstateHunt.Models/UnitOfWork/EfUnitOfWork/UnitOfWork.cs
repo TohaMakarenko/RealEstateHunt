c93b820a2dca86f5443a93bc8c033b3d8277cab2
Toha Makarenko <demo6112@gmail.com>
2018-05-16 11:57:23 +0300

    Unit of work

---
+++ RealEstateHunt.Models/UnitOfWork/EfUnitOfWork/UnitOfWork.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RealEstateHunt.Models.Repositories;
+using RealEstateHunt.Models.Repositories.EfRepositories;
+
+namespace RealEstateHunt.Models.UnitOfWork.EfUnitOfWork
+{
+    class UnitOfWork : IUnitOfWork
+    {
+        private readonly RehDbContext _dbContext;
+
+        private ICityRepository _cityRepository;
+        private IContactCommunicationRepository _contactCommunicationRepository;
+        private IContactRepository _contactRepository;
+        private IContractRepository _contractRepository;
+        private IDistrictRepository _districtRepository;
+        private IEmployeeRepository _employeeRepository;
+        private IOfferRepository _offerRepository;
+        private IRealEstateRepository _realEstateRepository;
+        private IRealEstateTypeRepository _realEstateTypeRepository;
+        private IUserRepository _userRepository;
+
+        public ICityRepository CityRepository =>
+            _cityRepository ?? (_cityRepository = new CityRepository(_dbContext));
+
+        public IContactCommunicationRepository ContactCommunicationRepository =>
+            _contactCommunicationRepository ?? (_contactCommunicationRepository = new ContactCommunicationRepository(_dbContext));
+
+        public IContactRepository ContactRepository =>
+            _contactRepository ?? (_contactRepository = new ContactRepository(_dbContext));
+
+        public IContractRepository ContractRepository =>
+            _contractRepository ?? (_contractRepository = new ContractRepository(_dbContext));
+
+        public IDistrictRepository DistrictRepository =>
+            _districtRepository ?? (_districtRepository = new DistrictRepository(_dbContext));
+
+        public IEmployeeRepository EmployeeRepository =>
+            _employeeRepository ?? (_employeeRepository = new EmployeeRepository(_dbContext));
+
+        public IOfferRepository OfferRepository =>
+            _offerRepository ?? (_offerRepository = new OfferRepository(_dbContext));
+
+        public IRealEstateRepository RealEstateRepository =>
+            _realEstateRepository ?? (_realEstateRepository = new RealEstateRepository(_dbContext));
+
+        public IRealEstateTypeRepository RealEstateTypeRepository =>
+            _realEstateTypeRepository ?? (_realEstateTypeRepository = new RealEstateTypeRepository(_dbContext));
+
+        public IUserRepository UserRepository =>
+            _userRepository ?? (_userRepository = new UserRepository(_dbContext));
+
+        public UnitOfWork(RehDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
---
+++ RealEstateHunt.Models/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RealEstateHunt.Models.Repositories;
+
+namespace RealEstateHunt.Models.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        ICityRepository CityRepository { get; }
+        IContactCommunicationRepository ContactCommunicationRepository { get; }
+        IContactRepository ContactRepository { get; }
+        IContractRepository ContractRepository { get; }
+        IDistrictRepository DistrictRepository { get; }
+        IEmployeeRepository EmployeeRepository { get; }
+        IOfferRepository OfferRepository { get; }
+        IRealEstateRepository RealEstateRepository { get; }
+        IRealEstateTypeRepository RealEstateTypeRepository { get; }
+        IUserRepository UserRepository { get; }
+    }
+}
